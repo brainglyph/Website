a40a82e0583c01ea0a48e9cf5131689e8b48876b
Giorgio Perri <giorgio.perri098@gmail.com>
2026-03-05 15:52:19 +0100

    Pitch deck redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/pitchdeck',
+        destination: 'https://www.figma.com/deck/mywjSzASueyMwqijQHbwao/Heli.os---Public-Pitch-Deck',
+        permanent: true,
+      },
+    ];
+  },
+}
  
 module.exports = nextConfig
