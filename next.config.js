b7f88078e1dec3484b8f51f83fddbb1c3103ad1d
Giorgio Perri <giorgio.perri098@gmail.com>
2026-03-24 17:45:28 +0100

    Added investor deck link

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         destination: 'https://www.figma.com/deck/mywjSzASueyMwqijQHbwao/Heli.os---Public-Pitch-Deck',
         permanent: true,
       },
+      {
+        source: '/investordeck',
+        destination: 'https://www.figma.com/deck/GxAn7wldqCeg0hgMIEjP9E/Heli.os---Investor-Standalone-Deck',
+        permanent: true,
+      }
     ];
   },
 }
